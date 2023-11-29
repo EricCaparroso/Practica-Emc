1fdab3e1e34699dcd1bda3b1cb65023944054eb2
EricCaparroso <126243096+EricCaparroso@users.noreply.github.com>
2023-11-29 10:46:46 +0000

    fix welcome

--- App.jsx
+++ App.jsx
@@ -1,11 +1,28 @@
+// App.js
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import Header from './components/header';
+import WelcomeScreen from './components/welcomeScreen';
+
 export default function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  const handleWelcomeDismiss = () => {
+    setShowWelcome(false);
+  };
+
   return (
     <View style={styles.container}>
-     <Header  /> 
-      <Text>app</Text>
+      <Header />
+      {showWelcome ? (
+        <WelcomeScreen onLoginPress={handleWelcomeDismiss} />
+      ) : (
+        <View style={styles.mainContent}>
+          <Text>app</Text>
+          {/* Otro contenido de la pantalla principal */}
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,10 +31,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 25,
+  },
+  mainContent: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'column', 
-    justifyContent: 'flex-start',marginTop: 25,
   },
 });
